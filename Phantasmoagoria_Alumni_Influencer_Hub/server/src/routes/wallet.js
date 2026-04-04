3ea56402340a2f7cfaf00cfb6d138c84b109bd50
Andrea Sharon Silva <sharonandrea245@gmail.com>
2026-04-04 18:05:17 +0530

    Removed unwanted files

--- Phantasmoagoria_Alumni_Influencer_Hub/server/src/routes/wallet.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const ctrl    = require('../controllers/walletController');
-const { authenticate } = require('../middleware/Auth');
-
-const router = express.Router();
-
-router.get('/', authenticate, ctrl.getWallet);
-
-module.exports = router;
